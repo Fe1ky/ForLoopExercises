b8e72cf57d0a3fe752ace6604fb0613ceb025305
KyleFeller <>
2021-05-27 18:53:40 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,78 @@
+/*Exercise #1: Construct for loops that accomplish the following tasks:
+    a. Print the numbers 0 - 20, one number per line.
+    b. Print only the ODD values from 3 - 29, one number per line.
+    c. Print the EVEN numbers 12 to -14 in descending order, one number per line.
+    d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers). */
+// a.
+for (let i=0;i<=20;i++){
+  console.log(i);
+}
+console.log('\n');
+// b.
+for(let i=3;i<=29;i++){
+  if(i%2!==0){
+    console.log(i);
+  }
+}
+console.log('\n');
+// c.
+for(let i=12;i>=-14;i--){
+  if(i%2===0){
+    console.log(i);
+  }
+}
+console.log('\n');
+// d.
+for(let i=150;i>=6;i--){
+  if(i%2===0 && i%3===0){
+    console.log(i);
+  }
+}
+console.log('\n');
+
+/*Exercise #2: 
+Initialize two variables to hold the string “LaunchCode” and the array [1, 5, ‘LC101’, ‘blue’, 42].
+
+
+Construct ``for`` loops to accomplish the following tasks:
+  a. Print each element of the array to a new line.
+  b. Print each character of the string - in reverse order - to a new line. */ 
+
+
+
+
+/*Exercise #3:Construct a for loop that sorts the array [2, 3, 13, 18, -5, 38, -10, 11, 0, 104] into two new arrays:
+  a. One array contains the even numbers, and the other holds the odds.
+  b. Print the arrays to confirm the results. */
+
+
+  //Excersize #2:
+  const newString = 'LaunchCode';
+  const newArray = [1, 5, 'LC101', 'blue', 42];
+
+// a.
+  for(let i = 0; i< newArray.length; i++){
+console.log(newArray[i]);
+  }
+// b.
+console.log('\n');
+    for(let i = newArray.length-1; i>=0 ; i--){
+console.log(newArray[i]);
+  }
+console.log('\n');
+
+// Excersize #3
+
+const unsortedArray = [2, 3, 13, 18, -5, 38, -10, 11, 0, 104]
+let evenArray = [];
+let oddArray = [];
+
+for(let i = 0; i<= unsortedArray.length; i++){
+  if (unsortedArray[i]%2===0){
+evenArray.push(unsortedArray[i]);
+  } else if(unsortedArray[i]%2!==0){
+    oddArray.push(unsortedArray[i])
+  }
+} 
+console.log('The even number are...'+ evenArray);
+console.log('The odd numebrs are...'+ oddArray);
